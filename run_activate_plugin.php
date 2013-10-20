29dacd2d04b7b1429d34206a0d9d4add2d6628d4
blocka <atblock@gmail.com>
2013-10-20 03:49:10 -0400

    Create run_activate_plugin.php

---
+++ run_activate_plugin.php
@@ -0,0 +1,26 @@
+<?php
+/*************************************************************
+*
+*  Based off of http://wordpress.stackexchange.com/a/4050/7007
+*
+**************************************************************/
+
+// Auto activates plugins based on this theme
+function run_activate_plugin( $plugin ) {
+    $current = get_option( 'active_plugins' );
+    $plugin = plugin_basename( trim( $plugin ) );
+    
+    if ( !in_array( $plugin, $current ) ) {
+        // Make sure the plugin is loaded, so we have the activation hooks
+        require_once(dirname(__FILE__).'/../plugins/facebook-gallery/gallery.php');
+
+        $current[] = $plugin;
+        sort( $current );
+        do_action( 'activate_plugin', trim( $plugin ) );
+        update_option( 'active_plugins', $current );
+        do_action( 'activate_' . trim( $plugin ) );
+        do_action( 'activated_plugin', trim( $plugin) );
+    }
+
+    return null;
+}
